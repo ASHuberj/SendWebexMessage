a2eb8f10003cc7f607f64e07fb0241cabe67eff8
Joseph Huber <joseph.huber@aspiria.com>
2024-07-04 09:12:31 +0200

    thrid commit, or at least what it should have been :(

--- index.php
+++ index.php
@@ -1,21 +1,28 @@
 <?php
-
-    try{
-        require("./SendWebexMessage.php");
-        
+/**
+ * Sends message to webex user.
+ *
+ * @category Project
+ * @package  Ka
+ * @author   Joseph Huber <joseph.huber@aspiria.com>
+ * @license  https://pseudolicense.com pseudolicense
+ * @link     Ka
+ */
+try{
+        include "./SendWebexMessage.php";
         echo "Email: ";
         $email = trim(fgets(STDIN));
         echo "Message: ";
         $message = trim(fgets(STDIN));
-        
-        $SendMsg = new SendWebexMessage($email ,$message);
+        $SendMsg = new SendWebexMessage();
+        $SendMsg->setMail($email);
+        $SendMsg->setMessage($message);
         $SendMsg->send();
-    
-    }
-    catch(Exception $e){
-        echo $e->getMessage()."\n";
-    }
-    finally{
+}
+catch(Exception $e){
+    echo $e->getMessage()."\n";
+}
+finally{
         
-    }
+}
 ?>
